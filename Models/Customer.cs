d0231356d3909bf898eef0b964959d08218c6c20
pujannagari-pallavi <pujannagaripallavi@gmail.com>
2025-08-05 18:22:41 +0530

    firstCommit

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirst.Models;
+
+public partial class Customer
+{
+    public decimal CustId { get; set; }
+
+    public string CustName { get; set; } = null!;
+
+    public decimal CustAge { get; set; }
+
+    public decimal? CustPhone { get; set; }
+}
---
+++ Models/DbEfCoreContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirst.Models;
+
+public partial class DbEfCoreContext : DbContext
+{
+    public DbEfCoreContext()
+    {
+    }
+
+    public DbEfCoreContext(DbContextOptions<DbEfCoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+#warning
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustId);
+
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.CustId)
+                .HasColumnType("numeric(18, 0)")
+                .HasColumnName("cust_id");
+            entity.Property(e => e.CustAge)
+                .HasColumnType("numeric(18, 0)")
+                .HasColumnName("cust_age");
+            entity.Property(e => e.CustName)
+                .HasMaxLength(50)
+                .HasColumnName("cust_name");
+            entity.Property(e => e.CustPhone)
+                .HasColumnType("numeric(18, 0)")
+                .HasColumnName("cust_phone");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
